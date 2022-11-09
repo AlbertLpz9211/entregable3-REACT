28e2d422405265f0df0486b738057b6ac597c490
Alberto Lopez Garcia <albertolopezgarcia@MacBook-Pro-de-Alberto.local>
2022-11-08 23:20:11 -0700

    get charters of API

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
 import { useEffect } from "react";
-import axios from "axios";
+import axios, { Axios } from "axios";
+import ResidentsTarjet from "./components/ResidentsTarjet";
 
 function App() {
   const [location, setLocation] = useState({});
-  
 
   useEffect(() => {
     const randomId = Math.floor(Math.random() * 126) + 1;
@@ -14,12 +14,20 @@ function App() {
       .get(`https://rickandmortyapi.com/api/location/${randomId}`)
       .then((res) => setLocation(res.data));
   }, []);
-  
-  console.log(location);
+
+  const seachLocation = () => {
+    axios.get();
+  };
+
+  //console.log(location);
 
   return (
     <div className="App">
-      <h1>cuerpo</h1>
+      <input type="text" name="" id="" placeholder="type a location ID" />
+      <button>Search</button>
+
+      {location.residents?.map(charters => (console.log(charters)))}
+      <ResidentsTarjet />
     </div>
   );
 }
---
+++ src/components/ResidentsTarjet.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ResidentsTarjet = () => {
+    return (
+        <div className='charterTarjet'>
+            <h1>modulo de personaje tarjet</h1>
+        </div>
+    );
+};
+
+export default ResidentsTarjet;
